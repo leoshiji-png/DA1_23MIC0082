f607280ad7bdc5102b30bf9b3470ecb1f629357f
LeoShiji <leo.shiji2023@vitstudent.ac.in>
2026-01-11 23:38:05 +0530

    Initial commit - Added Java program

---
+++ DA.java
@@ -0,0 +1,7 @@
+public class DA {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Leo Shiji");
+        System.out.println("Register Number: 23MIC0082");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
